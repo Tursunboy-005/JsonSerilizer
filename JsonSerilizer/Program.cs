daf935c85d8a0964c40a2937ed6beb2af349685f
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-21 10:51:39 +0500

    calculate

--- JsonSerilizer/Program.cs
+++ JsonSerilizer/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+            Console.Write("Enter mark: ");
+            string mark = Console.ReadLine();
+
+            if(mark == "+")
+            {
+                Calculate(input);
+            }else if(mark == "*")
+            {
+                Product(input);
+            }
+
+        }
+        static void Calculate(int number)
+        {
+            int resultCalculate = 0;
+            
+                for(int i = 0; i <= number; i++)
+                {
+                    resultCalculate += i;
+                }
+                Console.WriteLine($"Result: {resultCalculate}");
+            
+        }
+        static void Product(int number)
+        {
+            int resultProduct = 1;
+
+            
+                for (int i = 1; i <= number; i++)
+                {
+                    resultProduct *= i;
+                }
+                Console.WriteLine($"Result: {resultProduct}");
+            
         }
     }
 }
